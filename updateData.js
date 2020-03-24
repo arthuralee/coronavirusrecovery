dcb391f7e7fb0f1cc814a4258939089e463344fe
Arthur Lee <arthur@arthurlee.me>
2020-03-23 21:00:38 -0700

    Update data and data source

--- updateData.js
+++ updateData.js
@@ -3,7 +3,7 @@ const parser = require("csv-parse")();
 const fs = require("fs");
 
 const DATA_URL =
-  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
 function getInc(last15) {
   const result = [];
